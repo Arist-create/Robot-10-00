0e662593b9c28b6679cdad73c3f6129453b65e1a
Arist-create <89880441+Arist-create@users.noreply.github.com>
2022-10-18 02:30:20 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const refreshToken = '612d99e9-4de1-402d-87a9-a05e3cce4fdd'
+const refreshToken = x
 import request from 'request'
 import fetch from 'node-fetch'
 import { long, short } from './baza.js'
